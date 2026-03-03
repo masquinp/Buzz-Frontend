93b218193087d76d72c4eb55f417e7bf4145b3b6
pierre masquin <masquin.p13@gmail.com>
2026-03-03 14:05:39 +0100

    1

---
+++ screens/ReviewScreen.js
@@ -0,0 +1,33 @@
+
+/*
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from 'react';
+
+export default function ReviewScreen() {
+const [note, setNote] = useState()
+const [message, setMessage] = useState()
+const [reviews, setReviews] = useState()
+
+    const reviewBox = => (
+        <View style={styles.boxCard}>
+            <Image source={} styles={style.photo} />
+          <View styles={style.contentCard}>
+                <Text style={styles.nameText}>{.name}</Text>
+            <View style={styles.Box}>
+              <Text style={styles.noteText}>{.note}/5</Text>
+              <Text style={styles.dateText}>{.date}</Text>
+            </View>
+              <Text style={styles.messageText}>{.text}</Text>
+          </View>   
+        </View>
+    );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.evaluation}> Mes évaluations </View>
+      <View style={styles.note}> Note </View>
+      <View style={styles.listeBox}> Box </View>
+    </View>
+  );
+}
+*/
---
+++ screens/RideScreen.js
@@ -0,0 +1,27 @@
+/*
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function ReviewScreen() {
+    const reviewBox = => (
+        <View style={styles.boxCard}>
+            <Image source={} styles={style.photo} />
+          <View styles={style.firstCard}>
+                <Text style={styles.nameText}>{.name}</Text>
+                <Text style={styles.carText}>{.car}</Text>
+            <View style={styles.secondCard}>
+              <Text style={styles.noteText}>{.note}/5</Text>
+              <Text style={styles.dateText}>{.date}</Text>
+              <Text style={styles.priceText}>{.price}</Text>
+            </View>
+          </View>   
+        </View>
+    );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.evaluation}> Trajets Disponible </View>
+      <View style={styles.listeBox}> Box </View>
+    </View>
+  );
+}
+*/
